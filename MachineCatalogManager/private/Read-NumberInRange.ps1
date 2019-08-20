783eb969fc4f9e2e9ed716f2d9cfc1f63e853b5b
Corey Keller <corey@keller.net>
2019-08-20 14:39:14 -0500

    Change Write-Host to Write-Output

--- MachineCatalogManager/private/Read-NumberInRange.ps1
+++ MachineCatalogManager/private/Read-NumberInRange.ps1
@@ -1,6 +1,6 @@
 function Read-NumberInRange
 {
-  <#
+    <#
   .SYNOPSIS
     Will get input from the user and ensure that it is an acceptable numeric value.
   .DESCRIPTION
@@ -32,65 +32,75 @@ function Read-NumberInRange
   .NOTES
     General notes
   #>
-  param (
-    # Parameter help description
-    [Parameter(ParameterSetName="Range")]
-    [uint16] $Range,
-    # Parameter help description
-    [Parameter(ParameterSetName="ExplicitMinMax")]
-    [int16] $Min = [int16]::MinValue,
-    [Parameter(ParameterSetName="ExplicitMinMax")]
-    [int16] $Max = [int16]::MaxValue,
-    [switch] $AllowZero
+    param (
+        # Parameter help description
+        [Parameter(ParameterSetName = "Range")]
+        [uint16] $Range,
+        # Parameter help description
+        [Parameter(ParameterSetName = "ExplicitMinMax")]
+        [int16] $Min = [int16]::MinValue,
+        [Parameter(ParameterSetName = "ExplicitMinMax")]
+        [int16] $Max = [int16]::MaxValue,
+        [switch] $AllowZero
 
-  )
-  #Write-Host "PSBoundParameters:"
-  #$PSBoundParameters.Keys.foreach{Write-Host "$_ = $($PSBoundParameters[$_])"}
+    )
 
-  if ($Range) {
-    $min = $Range * - 1 # Since $Range is an unsigned int it will always be positive, so no need to test it as below
-    $max = $Range
-  }
-  elseif ($Min -gt $Max) {
-    $tempNum = $Min
-    $Min = $Max
-    $Max = $tempNum
-    $PSBoundParameters["Min"] = $Min
-    $PSBoundParameters["Max"] = $Max
-  }
-
-  Write-Host "[NOTE]: Negative numbers will leave that many servers out of Maintenance Mode, and put the rest in Maintenance Mode"
-  try {
-    [int16]$ChosenNumber = Read-Host -Prompt "Please enter a new number"
-    if (-not (($ChosenNumber -ge $Min) -and ($ChosenNumber -le $Max))) {
-      throw "Value was either too large or too small for the required range"
+    if ($Range)
+    {
+        $min = $Range * - 1 # Since $Range is an unsigned int it will always be positive, so no need to test it as below
+        $max = $Range
     }
-    elseif (($ChosenNumber -eq 0) -and (-not $AllowZero)){
-      throw "Zero is not an allowed value"
+    elseif ($Min -gt $Max)
+    {
+        $tempNum = $Min
+        $Min = $Max
+        $Max = $tempNum
+        $PSBoundParameters["Min"] = $Min
+        $PSBoundParameters["Max"] = $Max
     }
 
-  }
-  catch [System.Management.Automation.ArgumentTransformationMetadataException], [System.Management.Automation.RuntimeException] {
-    $Caught = $_
-    if ($Caught.Exception.Message -imatch  "Value was either too large or too small for") {
-      if (-not $AllowZero) {
-        Write-Output "ERROR: Input was out of bounds (i.e. not between $min and $max excluding 0). Please try again."
-      }
-      else{
-        Write-Output "ERROR: Input was out of bounds (i.e. not between $min and $max). Please try again."
-      }
+    Write-Output "[NOTE]: Negative numbers will leave that many servers out of Maintenance Mode, and put the rest in Maintenance Mode"
+    try
+    {
+        [int16]$ChosenNumber = Read-Host -Prompt "Please enter a new number"
+        if (-not (($ChosenNumber -ge $Min) -and ($ChosenNumber -le $Max)))
+        {
+            throw "Value was either too large or too small for the required range"
+        }
+        elseif (($ChosenNumber -eq 0) -and (-not $AllowZero))
+        {
+            throw "Zero is not an allowed value"
+        }
+
     }
-    elseif ($Caught.Exception.Message -imatch  "Input string was not in a correct format") {
-      Write-Output "ERROR: Input was not a number. Please try again."
+    catch [System.Management.Automation.ArgumentTransformationMetadataException], [System.Management.Automation.RuntimeException]
+    {
+        $Caught = $_
+        if ($Caught.Exception.Message -imatch "Value was either too large or too small for")
+        {
+            if (-not $AllowZero)
+            {
+                Write-Output "ERROR: Input was out of bounds (i.e. not between $min and $max excluding 0). Please try again."
+            }
+            else
+            {
+                Write-Output "ERROR: Input was out of bounds (i.e. not between $min and $max). Please try again."
+            }
+        }
+        elseif ($Caught.Exception.Message -imatch "Input string was not in a correct format")
+        {
+            Write-Output "ERROR: Input was not a number. Please try again."
+        }
+        elseif ($Caught.Exception.Message -imatch "Zero is not an allowed value")
+        {
+            Write-Output "ERROR: Input was out of bounds (i.e. not between $min and $max excluding 0). Please try again."
+        }
+        else
+        {
+            Write-Output "An unknown error occured. Exiting"
+            exit 77
+        }
+        Read-NumberInRange @PSBoundParameters
     }
-    elseif ($Caught.Exception.Message -imatch  "Zero is not an allowed value") {
-      Write-Output "ERROR: Input was out of bounds (i.e. not between $min and $max excluding 0). Please try again."
-    }
-    else {
-      Write-Output "An unknown error occured. Exiting"
-      exit 77
-    }
-    Read-NumberInRange @PSBoundParameters
-  }
-  return $ChosenNumber
+    return $ChosenNumber
 }
