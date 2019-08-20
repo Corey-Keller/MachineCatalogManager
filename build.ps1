2d525275d435693b7e546bf3f459ef71a5f8a24f
Corey Keller <corey@keller.net>
2019-08-20 18:56:57 -0500

    !deploy test psdeploy

--- build.ps1
+++ build.ps1
@@ -1,46 +1,13 @@
-<#
-.Description
-Installs and loads all the required modules for the build.
-Derived from scripts written by Warren F. (RamblingCookieMonster)
-#>
-
-[cmdletbinding()]
 param ($Task = 'Default')
 
-Write-Output "Starting build"
-
-if (-not (Get-PackageProvider | ? Name -eq nuget))
-{
-    Write-Output "  Install Nuget PS package provider"
-    Install-PackageProvider -Name NuGet -Force -Confirm:$false | Out-Null
-}
-
-$publishRepository = 'PSGallery'
-
 # Grab nuget bits, install modules, set build variables, start build.
-Write-Output "  Install And Import Build Modules"
-$psDependVersion = '0.1.62'
-if (-not(Get-InstalledModule PSDepend -RequiredVersion $psDependVersion -EA SilentlyContinue))
-{
-    Install-Module PSDepend -RequiredVersion $psDependVersion -Force -Scope CurrentUser
-}
-Import-Module PSDepend -RequiredVersion $psDependVersion
-Invoke-PSDepend -Path "$PSScriptRoot\build.depend.psd1" -Install -Import -Force
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-if (-not (Get-Item env:\BH*)) 
-{
-    Set-BuildEnvironment
-    Set-Item env:\PublishRepository -Value $publishRepository
-}
-. "$PSScriptRoot\tests\Unload-SUT.ps1"
+Install-Module Psake, PSDeploy, BuildHelpers, platyPS, PSScriptAnalyzer -force
+Install-Module Pester -Force -SkipPublisherCheck
+Import-Module Psake, BuildHelpers, platyPS, PSScriptAnalyzer
 
-Write-Output "  InvokeBuild"
-Invoke-Build $Task -Result result
-if ($Result.Error)
-{
-    exit 1
-}
-else 
-{
-    exit 0
-}
+Set-BuildEnvironment
+
+Invoke-psake -buildFile .\psake.ps1 -taskList $Task -nologo
+exit ([int](-not $psake.build_success))
